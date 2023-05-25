c9cfc723421f215e8774cde75f318a535188b744
Mortaza Hassani <95752498+MortazaHassani@users.noreply.github.com>
2023-05-25 11:01:12 +0500

    Dual motor A,B
    Added forward, backward, stop control using serial monitor

--- Nodemcu/singleMotor/singleMotor.ino
+++ Nodemcu/singleMotor/singleMotor.ino
@@ -1,6 +1,9 @@
-int ENA = D2;
-int IN1 = D3;
-int IN2 = D4;
+int ENA = 14;
+int IN1 = 12;
+int IN2 = 13;
+int IN3 = 0;
+int IN4 = 2;
+int ENB = 15;
 
 void setup() {
   Serial.begin(115200); 
@@ -10,46 +13,64 @@ void setup() {
   pinMode(IN2, OUTPUT); 
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT); 
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop() {
+
+  Serial.println("Enter direction:");
+  while (Serial.available() == 0) {}     //wait for data available
+  String teststr = Serial.readString();  //read until timeout
+  teststr.trim();                        // remove any \r \n whitespace at the end of the String
+  if (teststr == "forward") {
+    Serial.println("forward");
+    forward();
+  } 
+  else if(teststr == "backward"){
+    Serial.println("backward");
+    backward();
+  }
+  else {
+    stop();
+  }
+  /*
   setDirection();
   delay(1000);
   changeSpeed();
   delay(1000);
+  */
 }
-
-void setDirection() {
-  Serial.println("Set Direction!");
+void forward(){
+  Serial.println("Going forward");
   analogWrite(ENA, 255);
-
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  delay(5000);
-  
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  delay(5000);
-  
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-}
 
-void changeSpeed() {
+  analogWrite(ENB, 255);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+}
+void backward(){
+  Serial.println("Going backward");
+  analogWrite(ENA, 255);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  
-  for (int i = 0; i < 256; i++) {
-    analogWrite(ENA, i);
-    delay(20);
-    Serial.println("<");
-  }
-  
-  for (int i = 255; i >= 0; --i) {
-    analogWrite(ENA, i);
-    delay(20);
-    Serial.println(">");
-  }
+
+  analogWrite(ENB, 255);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+}
+void stop(){
+  Serial.println("stop");
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
